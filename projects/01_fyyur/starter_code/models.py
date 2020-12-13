e4f2cc8d6ea53beb061de7bfffab840abf5b2240
nesreensada <nesreensada@gmail.com>
2020-12-13 14:15:49 +0200

    bug: fix the model and the migration codes

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -3,18 +3,23 @@
 #----------------------------------------------------------------------------#
 import json
 from flask import Flask
+from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
 migrate = Migrate(app, db)
@@ -32,12 +37,14 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String()), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_talent_description = db.Column(db.String())
+    website = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='Venue', lazy=True)
     # TODO: add functions needed as insert, update, delete, description
-    # TODO: Add DB relationship
     def __repr__(self):
       return f'<Artist {self.id} {self.name} >'
 
@@ -52,27 +59,21 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    # add the db relationship
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_talent_description = db.Column(db.String())
+    shows = db.relationship('Show', backref='Artist', lazy=True)
     # TODO: add functions needed as insert, update, delete, description
-    # TODO: Add DB relationship
     def __repr__(self):
       return f'<Venue {self.id} {self.name} >'
 
 #TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
     __tablename__ = 'Show'
-
     id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'),nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    # add the db relationship
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
     # TODO: add functions needed as insert, update, delete, description
-    # TODO: Add DB relationship
     def __repr__(self):
       return f'<Show {self.id}, Artist {self.artist_id}, Venue {self.venue_id} and Start Time {self.start_time}>'
