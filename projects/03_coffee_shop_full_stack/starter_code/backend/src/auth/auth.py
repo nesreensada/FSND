478e6e198b3eb633e7916ab48d742583426fa31e
nesreensada <nesreensada@gmail.com>
2021-01-13 17:38:58 +0200

    fix authorization file

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'fsnd-nes.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee-shop'
+API_AUDIENCE = 'coffe-shop-api'
 
 
 # AuthError Exception
